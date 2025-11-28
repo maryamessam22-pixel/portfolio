3b317f7525b2a8e2a32ecb96043801453b11fe2c
maryamessam22-pixel <maryamessam22@gmail.com>
2025-11-28 03:28:49 +0200

    EDIT AT ProjectsDetailsGraphic

--- src/pages/ProjectDetailsGraphicDesign.jsx
+++ src/pages/ProjectDetailsGraphicDesign.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Link, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
