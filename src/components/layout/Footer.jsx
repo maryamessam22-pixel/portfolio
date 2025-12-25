5b14d1a898b6f0447ecc26c121b683c44b2606f3
maryamessam22-pixel <maryamessam22@gmail.com>
2025-12-25 04:56:49 +0200

    edit at Upwork path icon

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -61,18 +61,21 @@ const Footer = () => {
           </a>
 
          
-           <a href="https://www.upwork.com/freelancers/~0155e845a47587b035"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="social-icon upwork"
-               >
-            <svg viewBox="0 0 24 24" fill="currentColor" aria-label="Upwork">
-            <path d="M17.5 6.5c-1.5 0-2.7.9-3.3 2.3-.6-1.3-1.3-2.6-1.9-3.8h-2v5.7c0 1.6-.8 2.7-2.2 2.7s-2.2-1.1-2.2-2.7V5H3.9v5.8c0 2.8 1.8 4.6 4.3 4.6 2.4 0 4.2-1.8 4.2-4.6V9.2c.4.9.8 1.7 1.2 2.5l-1.9 8.3h2.1l1.6-7.1c.4.1.9.2 1.4.2 2.7 0 4.7-1.9 4.7-4.7 0-2.6-1.8-4.6-4-4.6zm-.3 6.3c-.5 0-.9-.1-1.3-.3l.5-2.1c.3-1.3.9-2 1.9-2 1.1 0 1.8.9 1.8 2.1 0 1.4-1 2.3-2.9 2.3z"/>
-            </svg>
-          </a>
+            <a href="https://www.upwork.com/freelancers/~0155e845a47587b035"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-icon upwork">
+              <svg
+                viewBox="0 0 448 512"
+                fill="currentColor"
+                aria-label="Upwork"
+              >
+                <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM270.8 274.3c5.2 8.4 23.6 29.9 51.5 29.9v0c25.2 0 44.9-20.2 44.9-49.7s-19.4-49.7-44.9-49.7s-44.9 16.7-51.5 69.5zm-26.7-41.8c7.3-30.5 32.7-60.1 78.2-60.1l0 0c45.1 0 80.9 35.2 80.9 82.2s-35.9 81.9-80.9 81.9c-.6 0-1.1 0-1.7 0c-.5 0-1.1 0-1.6 0h-.1c-14.5-.5-28.7-4.8-40.9-12.6c-4.7-2.8-9.1-6-13.4-9.5l-12.8 78.4H214.9l19.4-110.6c-20.8-29.1-31.6-62.4-36.2-79.7V255c0 48-30.5 81.5-74.2 81.5c-22 0-41-8-54.8-23.3c-13.4-14.8-20.8-35.5-20.8-58.3V176.8H84.5l-.3 78.2c0 28.4 14.5 49.3 39.8 49.3s38.2-20.9 38.2-49.3V176.8h62.8c4.8 19.3 10.9 40.1 19.2 55.6z" />
+              </svg>
+            </a>
 
-           <a
-               href="https://www.freelancer.com/u/Mariam101128"
+
+           <a href="https://www.freelancer.com/u/Mariam101128"
                target="_blank"
                rel="noopener noreferrer"
                className="social-icon freelancer">
